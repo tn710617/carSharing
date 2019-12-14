f179de4aa406a9105e40d5a046ba2bfcb66bd4c8
Ray <tn710617@gmail.com>
2019-12-14 20:49:15 +0800

    Fix missing responses

--- app/Http/Resources/Post.php
+++ app/Http/Resources/Post.php
@@ -20,9 +20,11 @@ class Post extends JsonResource
             'id'             => $this->id,
             'user_id'        => $this->user_id,
             'seat'           => 6,
+            'subject'        => $this->subject,
             'departure_date' => Carbon::parse($this->departure_date)->format('m/d'),
             'departure'      => $this->departure,
             'destination'    => $this->destination,
+            'description'    => $this->description,
             'ptt_url'        => $this->ptt_url,
             'type'           => $this->type,
             'created_at'     => $this->created_at
